f0e563f009a18e4567bc25b4d170ef26bee9a2a8
Ange0608 <angelique.dartevelle@hotmail.fr>
2023-03-29 16:19:19 +0200

    finish to do

--- projet js/todo.js
+++ projet js/todo.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//list.innerHTML = localStorage.getItem("toDoList");//c'est le lien avec le localstorage en bas
+
+//Formulaire d'ajout de tâches
 form.onsubmit = () => {
     const li = document.createElement("li"); //créer un nouveau document
     const spanDel= document.createElement("span");//Ajoute un element 
@@ -12,11 +15,21 @@ form.onsubmit = () => {
     list.appendChild(li);
 
     add.value = ""; //Remettre le champs à 0
+    noTask.style.display ="none";
+
+//localStorage.setItem("toDoList", list.innerHTML)//Pour sauvegarder la liste
+
+
 
     return false; // Cela veut dire que c'est la dernière boucle de l'actioni
 
 }
-
+//Fonction supprimer
 function del(element){
     element.remove();
+
+    if(list.innerHTML == "")
+    {
+        noTask.style.display = "block";
+    }
 }
